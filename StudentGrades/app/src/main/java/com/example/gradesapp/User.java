cbc73030780a57be5bce7b207a9caa789a49b67b
townesean <seanmichaeltowne@gmail.com>
2020-02-04 20:27:58 -0800

    added User.java manually cuz git is stupid

---
+++ StudentGrades/app/src/main/java/com/example/gradesapp/User.java
@@ -0,0 +1,71 @@
+package com.example.gradesapp;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import com.example.gradesapp.DB.AppDatabase;
+
+@Entity(tableName = AppDatabase.USER_TABLE)
+public class User {
+    @PrimaryKey(autoGenerate = true)
+    private int key;
+
+    String username,password,firstName,lastName,userID;
+
+    public User(String inputUsername, String inputPassword,
+                String inputFirstName, String inputLastName,String inputUserID){
+        username = inputUsername;
+        password = inputPassword;
+        firstName = inputFirstName;
+        lastName = inputLastName;
+        userID = inputUserID;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+}
