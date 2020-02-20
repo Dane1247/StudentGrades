dc33a81b645a4946cad5a287c1421e9670ed047e
Janelle <juganiza@csumb.edu>
2020-02-20 13:45:17 -0800

    works with what we have

--- StudentGrades/app/src/main/java/com/example/gradesapp/MainActivity.java
+++ StudentGrades/app/src/main/java/com/example/gradesapp/MainActivity.java
@@ -59,7 +59,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, CreateAccountActivity.class));
-                finish();
             }
         });
 
