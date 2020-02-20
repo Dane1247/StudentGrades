c622aca0eee49831c10d2927283f3b5b2327d307
Maryann Cortez <marcortez@csumb.edu>
2020-02-19 19:02:03 -0800

    Should be better

--- StudentGrades/app/src/main/java/com/example/gradesapp/UserDashboard.java
+++ StudentGrades/app/src/main/java/com/example/gradesapp/UserDashboard.java
@@ -1,8 +1,7 @@
 package com.example.gradesapp;
 
-
 import android.content.Context;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -12,41 +11,109 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.List;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
+import com.example.gradesapp.DB.AppDatabase;
+import com.example.gradesapp.DB.CourseDAO;
+import com.example.gradesapp.DB.GradeDAO;
+import com.example.gradesapp.DB.UserDAO;
+import com.example.gradesapp.DB.EnrollmentDAO;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.Iterator;
+import java.util.List;
 
 public class UserDashboard extends AppCompatActivity {
 
-    private ArrayList<String> data = new ArrayList<String>();
+    private UserDAO userDAOReference;
+    private EnrollmentDAO enrollmentDAOReference;
+    private CourseDAO courseDAOReference;
+    private GradeDAO gradeDAOReference;
+    private List<String> data; // list of courses (name and grade)
+    private FloatingActionButton editUser;
+    private FloatingActionButton addCourse;
 
     @Override
-
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_activity);
-        ListView lv = (ListView) findViewById(R.id.listview);
-        generateListContent();
+        ListView lv = findViewById(R.id.listview);
+        editUser = findViewById(R.id.editUser);
+        addCourse = findViewById(R.id.addCourse);
+
+
+        final int userID = getIntent().getExtras().getInt("userID");
+        getDAOs();
+
+        List<Enrollment> courses = enrollmentDAOReference.getEnrollmentsWithStudentID(userID);
+
+        generateListContent(courses);
         lv.setAdapter(new MyListAdaper(this, R.layout.list_item, data));
+
+
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                //LIst item clicked
+                Intent intent = new Intent(view.getContext(), CourseDashboard.class);
+                intent.putExtra("courseID", "courseID");
+                intent.putExtra("userID", userID);
+                startActivity(intent);
             }
+        });
 
+        editUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), EditUser.class);
+                intent.putExtra("userID", userID);
+                startActivity(intent);
+            }
+        });
+
+        addCourse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), AddCourse.class);
+                intent.putExtra("userID", userID);
+                startActivity(intent);
+            }
         });
     }
 
-    private void generateListContent() {
-        for(int i = 0; i < 5; i++) {
-            data.add("Test" + i);
-            //add list of courses here
+    private void getDAOs() {
+        userDAOReference = Room.databaseBuilder(this, AppDatabase.class, AppDatabase.dbName)
+                .allowMainThreadQueries()
+                .build()
+                .getUserDAO();
+        enrollmentDAOReference = Room.databaseBuilder(this, AppDatabase.class, AppDatabase.dbName)
+                .allowMainThreadQueries()
+                .build()
+                .getEnrollmentDAO();
+        courseDAOReference = Room.databaseBuilder(this, AppDatabase.class, AppDatabase.dbName)
+                .allowMainThreadQueries()
+                .build()
+                .getCourseDAO();
+        gradeDAOReference = Room.databaseBuilder(this, AppDatabase.class, AppDatabase.dbName)
+                .allowMainThreadQueries()
+                .build()
+                .getGradeDAO();
+    }
+
+    private String getGrade(int courseid){
+        List<Grade> grade = gradeDAOReference.getGradeWithCourseID(courseid);
+        return grade.get(0).score;
+    }
+
+    private void generateListContent(List<Enrollment> courseid) {
+        Iterator iterator = courseid.iterator();
+        while(iterator.hasNext()) {
+            List<Course> course = (List<Course>) iterator.next();
+            List<Course> courses = courseDAOReference.getCoursesWithCourseID(course.get(0).getCourseID());
+            data.add(courses.get(0).getTitle() + getGrade(courses.get(0).getCourseID()));
         }
     }
 
@@ -59,7 +126,6 @@ public class UserDashboard extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-
         if (id == R.id.action_settings) {
             return true;
         }
@@ -69,6 +135,7 @@ public class UserDashboard extends AppCompatActivity {
     private class MyListAdaper extends ArrayAdapter<String> {
         private int layout;
         private List<String> mObjects;
+
         private MyListAdaper(Context context, int resource, List<String> objects) {
             super(context, resource, objects);
             mObjects = objects;
@@ -78,36 +145,23 @@ public class UserDashboard extends AppCompatActivity {
         @Override
         public View getView(final int position, View convertView, ViewGroup parent) {
             ViewHolder mainViewholder = null;
-
             if(convertView == null) {
                 LayoutInflater inflater = LayoutInflater.from(getContext());
                 convertView = inflater.inflate(layout, parent, false);
                 ViewHolder viewHolder = new ViewHolder();
-                viewHolder.thumbnail = (ImageView) convertView.findViewById(R.id.list_item_thumbnail);
-                viewHolder.title = (TextView) convertView.findViewById(R.id.list_item_text);
-                viewHolder.button = (Button) convertView.findViewById(R.id.list_item_btn);
+                viewHolder.title = convertView.findViewById(R.id.list_item_text);
                 convertView.setTag(viewHolder);
             }
 
             mainViewholder = (ViewHolder) convertView.getTag();
-            mainViewholder.button.setOnClickListener(new View.OnClickListener() {
-
-                @Override
-                public void onClick(View v) {
-                    //button for course was selected
-
-                }
-            });
             mainViewholder.title.setText(getItem(position));
             return convertView;
 
         }
-
     }
 
     public class ViewHolder {
         TextView title;
-        Button button;
-
     }
+
 }
